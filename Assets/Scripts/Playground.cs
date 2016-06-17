71278580d765f52676e5fc0525d7161d7b318178
Bruno Cicanci <brunocicanci@gmail.com>
2016-06-17 19:34:08 -0300

    Shooting ping pong balls

---
+++ Assets/Scripts/Playground.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playground : MonoBehaviour 
+{
+	public Vector3 bulletPosition;
+	public float bulletSpeed;
+	public GameObject bulletPrefab;
+
+	public float spaceshipSpeed;
+	
+	void Update () 
+	{
+		float h = Input.GetAxis("Horizontal") * Time.deltaTime * spaceshipSpeed;
+		float v = Input.GetAxis("Vertical") * Time.deltaTime * spaceshipSpeed;
+
+		transform.Translate(new Vector3(h, v, 0f));
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject bullet = Instantiate(bulletPrefab, bulletPosition, transform.rotation) as GameObject;
+			Rigidbody shot = bullet.GetComponent<Rigidbody>();
+			shot.AddForce(transform.forward * bulletSpeed);
+
+			Destroy(bullet, 3);
+		}
+	}
+}
