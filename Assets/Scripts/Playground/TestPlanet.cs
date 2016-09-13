23948f6bb8d3b4b5c48510339c645540460cf5dd
Bruno Cicanci <brunocicanci@gmail.com>
2016-09-12 23:51:22 -0300

    New scene for the UI playground

---
+++ Assets/Scripts/Playground/TestPlanet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlanet : MonoBehaviour
+{
+	void Update ()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime);
+    }
+}
