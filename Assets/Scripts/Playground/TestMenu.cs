03454f2f636358ef01e4df1bc6a2cba62807709f
Bruno Cicanci <brunocicanci@gmail.com>
2016-09-16 01:04:49 -0300

    Script to update inventory information

--- Assets/Scripts/Playground/TestMenu.cs
+++ Assets/Scripts/Playground/TestMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -6,9 +7,20 @@ namespace Playground
 {
     public class TestMenu : MonoBehaviour
     {
+		public Text shipNameLabel;
+		public Text shipClassLabel;
+		public Text shipLevelLabel;
+
+		public string shipName;
+		public string shipClass;
+		[Range (0, 100)]
+		public int shipLevel;
+
         void Start()
         {
             Debug.LogWarning("Playground::TestMenu script is in use by " + gameObject.name);
+
+			UpdateUI();
         }
 
         void Update()
@@ -25,6 +37,24 @@ namespace Playground
                 }
             }
         }
+
+		void UpdateUI() 
+		{
+			if (shipNameLabel != null)
+			{
+				shipNameLabel.text = shipName;
+			}
+
+			if (shipClassLabel != null)
+			{
+				shipClassLabel.text = shipClass;
+			}
+
+			if (shipLevelLabel != null)
+			{
+				shipLevelLabel.text = shipLevel.ToString();
+			}
+		}
     }
 
 }
