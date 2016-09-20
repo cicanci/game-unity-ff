8ae4b2b4c3ff79095ef98dca22612450c1c9b870
Bruno Cicanci <brunocicanci@gmail.com>
2016-09-20 01:54:59 -0300

    Adjusted UI objects movement

--- Assets/Scripts/Playground/TestInventory.cs
+++ Assets/Scripts/Playground/TestInventory.cs
@@ -12,7 +12,9 @@ namespace Playground
     public class TestInventory : MonoBehaviour
     {
         [Range(0, 100)]
-        public float movementSpeed = 0;
+        public float horizontalSpeed = 0;
+		[Range(0, 100)]
+		public float verticalSpeed = 0;
         public UIType type = UIType.None;
         public bool inverted = false;
 
@@ -23,46 +25,47 @@ namespace Playground
 
         void Update()
         {
-            Vector3 position = Vector3.zero;
-
             if (Input.GetAxis("Mouse X") < 0)
             {
-                //print("Mouse moved left");
-                position = ((inverted) ? Vector3.right : Vector3.left) * Time.deltaTime * movementSpeed;
+				StartCoroutine(moveObject(Vector3.left, Time.deltaTime * horizontalSpeed));
             }
 
             if (Input.GetAxis("Mouse X") > 0)
             {
-                //print("Mouse moved right");
-                position = ((inverted) ? Vector3.left : Vector3.right) * Time.deltaTime * movementSpeed;
+				StartCoroutine(moveObject(Vector3.right, Time.deltaTime * horizontalSpeed));
             }
 
             if (Input.GetAxis("Mouse Y") < 0)
             {
-                //print("Mouse moved down");
-                position = ((inverted) ? Vector3.up : Vector3.down) * Time.deltaTime * movementSpeed;
+				StartCoroutine(moveObject(((inverted) ? Vector3.up : Vector3.down), Time.deltaTime * verticalSpeed));
             }
 
             if (Input.GetAxis("Mouse Y") > 0)
             {
-                //print("Mouse moved up");
-                position = ((inverted) ? Vector3.down : Vector3.up) * Time.deltaTime * movementSpeed;
-            }
-
-            switch (type)
-            {
-                case UIType.UICanvas:
-                    gameObject.GetComponent<RectTransform>().localPosition += position;
-                    break;
-                case UIType.UIObject:
-                    gameObject.transform.localPosition += position;
-                    break;
-                case UIType.None:
-                    Debug.LogWarning("No UIType set for " + gameObject.name);
-                    break;
-                default:
-                    break;
+				StartCoroutine(moveObject(((inverted) ? Vector3.down : Vector3.up), Time.deltaTime * verticalSpeed));
             }
         }
+
+		IEnumerator moveObject(Vector3 direction, float speed)
+		{
+			Vector3 destination = Vector3.zero;
+
+			switch (type)
+			{
+			case UIType.UICanvas:
+				gameObject.GetComponent<RectTransform>().Translate(direction * speed);
+				break;
+			case UIType.UIObject:
+				gameObject.transform.Translate(direction * speed);
+				break;
+			case UIType.None:
+				Debug.LogWarning("No UIType set for " + gameObject.name);
+				break;
+			default:
+				break;
+			}
+
+			yield return null;
+		}
     }
 }
