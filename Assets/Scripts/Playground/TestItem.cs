cf34af56adc266dd93ee9cd7025c2c1234d5d40e
Bruno Cicanci <brunocicanci@gmail.com>
2016-09-27 15:15:41 -0300

    Left item slots showing up

---
+++ Assets/Scripts/Playground/TestItem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Playground
+{
+	public class TestItem : MonoBehaviour 
+	{
+		public RectTransform inventory;
+
+		private float offsetX = 390;
+		private float offsetY = 268;
+		private int itemPosition = 0;
+		private int itemHeight = 109;
+
+		void OnEnable()
+		{
+			Debug.Log("OnEnable");
+		}
+
+		void Update()
+		{
+			UpdatePosition();
+		}
+
+		public void ShowPanel(int item)
+		{
+			itemPosition = item * itemHeight;
+			UpdatePosition();
+
+			gameObject.SetActive(true);
+		}
+
+		public void HidePanel()
+		{
+			gameObject.SetActive(false);
+		}
+
+		private void UpdatePosition()
+		{
+			Vector2 position = new Vector2(inventory.localPosition.x - offsetX, inventory.localPosition.y + offsetY - itemPosition);
+			GetComponent<RectTransform>().localPosition = position;
+		}
+	}
+}
