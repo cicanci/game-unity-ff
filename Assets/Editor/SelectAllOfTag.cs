e4af5e200a8a9f20077231972b357ae493e5a555
Bruno Cicanci <brunocicanci@gmail.com>
2016-10-04 19:53:26 -0300

    Added script to select objects by tag from Unity live training

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard 
+{
+	public string searchTag = "Your tag here";
+
+	[MenuItem("My Tools/Select All Of Tag...")]
+	static void SelectAllOfTagWizard()
+	{
+		ScriptableWizard.DisplayWizard<SelectAllOfTag>("Select All Of Tag...", "Make Selection");
+	}
+
+	void OnWizardCreate()
+	{
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+		Selection.objects = gameObjects;
+	}
+}
