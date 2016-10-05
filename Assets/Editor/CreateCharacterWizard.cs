cef46194d21abebb813c3a1ba590fdced552031c
Bruno Cicanci <brunocicanci@gmail.com>
2016-10-04 21:24:38 -0300

    Another editor tool from Unity live training, keep it here to use later for asteroids

---
+++ Assets/Editor/CreateCharacterWizard.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateCharacterWizard : ScriptableWizard 
+{
+	public Texture2D portraitTexture;
+	public Color color = Color.white;
+	public string nickname = "Default nickname";
+
+	[MenuItem ("My Tools/Create Character Wizard")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard<CreateCharacterWizard> ("Create Character", "Create new", "Update selected");
+	}
+
+	void OnWizardCreate()
+	{
+//		GameObject characterGO = new GameObject ();
+//
+//		Character characterComponent = characterGO.AddComponent<Character> ();
+//		characterComponent.portrait = portraitTexture;
+//		characterComponent.color = color;
+//		characterComponent.nickname = nickname;
+//
+//		PlayerMovement playerMovement = characterGO.AddComponent<PlayerMovement> ();
+//		characterComponent.playerMovement = playerMovement;
+	}
+
+	void OnWizardOtherButton()
+	{
+		if (Selection.activeTransform != null)
+		{
+//			Character characterComponent = Selection.activeTransform.GetComponent<Character>();
+//
+//			if (characterComponent != null)
+//			{
+//				characterComponent.portrait = portraitTexture;
+//				characterComponent.color = color;
+//				characterComponent.nickname = nickname;
+//			}
+		}
+	}
+
+	void OnWizardUpdate()
+	{
+		helpString = "Enter character details";
+	}
+}
--- Assets/Editor/SelectAllOfTag.cs
+++ Assets/Editor/SelectAllOfTag.cs
@@ -6,10 +6,10 @@ public class SelectAllOfTag : ScriptableWizard
 {
 	public string searchTag = "Your tag here";
 
-	[MenuItem("My Tools/Select All Of Tag...")]
+	[MenuItem("My Tools/Select All Of Tag")]
 	static void SelectAllOfTagWizard()
 	{
-		ScriptableWizard.DisplayWizard<SelectAllOfTag>("Select All Of Tag...", "Make Selection");
+		ScriptableWizard.DisplayWizard<SelectAllOfTag>("Select All Of Tag", "Make Selection");
 	}
 
 	void OnWizardCreate()
