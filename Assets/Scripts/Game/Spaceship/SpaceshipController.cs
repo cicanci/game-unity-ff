7c2a575214e69017ca045b2a849ad647624bf140
Bruno Cicanci <brunocicanci@gmail.com>
2017-05-11 21:39:03 -0300

    Spaceship controller script

---
+++ Assets/Scripts/Game/Spaceship/SpaceshipController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalFrontier.Game.Spaceship
+{
+    public class SpaceshipController : MonoBehaviour 
+    {
+        public float MovementSpeed;
+
+        private void Start() 
+        {
+
+        }
+
+        private void Update() 
+        {
+            SpaceshipRotation();
+        }
+
+        private void SpaceshipRotation()
+        {
+            float h = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+            float v = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+            transform.Rotate(v, h, 0);
+        }
+    }
+}
