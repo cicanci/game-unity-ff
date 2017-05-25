f1222b24d3f043614af1b3f083ebd34c67766e5a
Bruno Cicanci <brunocicanci@gmail.com>
2017-05-24 22:35:28 -0300

    Fixed corrupted sprites

--- Assets/Editor/SpriteProcessor.cs
+++ Assets/Editor/SpriteProcessor.cs
@@ -6,15 +6,15 @@ public class SpriteProcessor : AssetPostprocessor
 {
 	void OnPostprocessTexture(Texture2D texture)
 	{
-		string lowerCaseAssetPath = assetPath.ToLower();
-		bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
+		//string lowerCaseAssetPath = assetPath.ToLower();
+		//bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
 
-		if (isInSpritesDirectory)
-		{
-			TextureImporter textureImporter = (TextureImporter)assetImporter;
-			textureImporter.textureType = TextureImporterType.Sprite;
+		//if (isInSpritesDirectory)
+		//{
+		//	TextureImporter textureImporter = (TextureImporter)assetImporter;
+		//	textureImporter.textureType = TextureImporterType.Sprite;
 
-			Debug.Log("Texture2D converted to Sprite");
-		}
+		//	Debug.Log("Texture2D converted to Sprite");
+		//}
 	}
 }
--- Assets/Scripts/Game/Spaceship/SpaceshipController.cs
+++ Assets/Scripts/Game/Spaceship/SpaceshipController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,9 @@ namespace FinalFrontier.Game.Spaceship
 {
     public class SpaceshipController : MonoBehaviour 
     {
+        [Range(0.1f, 1f)]
+        public float Bias;
+        public Vector3 CameraOffset;
         public float MovementSpeed;
 
         private void Start() 
@@ -16,6 +20,20 @@ namespace FinalFrontier.Game.Spaceship
         private void Update() 
         {
             SpaceshipRotation();
+            SpaceShipMovement();
+        }
+
+        private void LateUpdate()
+        {
+            CameraFollow();
+        }
+
+        private void CameraFollow()
+        {
+            Vector3 moveCamTo = transform.position - transform.forward * CameraOffset.z + Vector3.up * CameraOffset.y;
+            Camera.main.transform.position = Camera.main.transform.position * Bias + moveCamTo * (1 - Bias);
+            Camera.main.transform.rotation = transform.rotation;
+            //Camera.main.transform.LookAt(transform.position + transform.forward * MovementSpeed);
         }
 
         private void SpaceshipRotation()
@@ -23,6 +41,11 @@ namespace FinalFrontier.Game.Spaceship
             float h = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
             float v = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
             transform.Rotate(v, h, 0);
+        }
+
+        private void SpaceShipMovement()
+        {
+            transform.position += transform.forward * Time.deltaTime * MovementSpeed;
         }
     }
 }
