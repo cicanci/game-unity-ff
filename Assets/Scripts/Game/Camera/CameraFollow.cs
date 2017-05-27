ded40281bbb35c33351546a5f62ee17379d646a4
Bruno Cicanci <brunocicanci@gmail.com>
2017-05-27 19:56:44 -0300

    FPS counter prefab and script

--- Assets/Scripts/Game/Camera/CameraFollow.cs
+++ Assets/Scripts/Game/Camera/CameraFollow.cs
@@ -13,43 +13,43 @@ namespace FinalFrontier.Game.Camera
 
         public float TargetDistance;
 
-        //public Vector3 Offset;
+        public Vector3 Offset;
 
-        private void Start()
+        private void Awake()
         {
-            if (TargetToFollow == null) 
+            if (TargetToFollow != null)
             {
-                GameObject player = GameObject.FindGameObjectWithTag("Player");
-                if (player == null)
-                {
-                    Debug.LogError("No target object set or player tag found to follow");
-                }
-                else
-                {
-                    TargetToFollow = player.transform;
-                }
+                return;
+            }
+            
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (player == null)
+            {
+                Debug.LogError("No target object set or player tag found to follow");
+            }
+            else
+            {
+                TargetToFollow = player.transform;
             }
         }
 
         private void Update() 
         {
-            if (TargetToFollow != null)
+            if (TargetToFollow == null)
             {
-                Vector3 velocity = Vector3.zero;
-                Vector3 forward = TargetToFollow.forward * TargetDistance;
-
-                // Offset is not working properly, I'll check that later
-                //Vector3 targetPosition = TargetToFollow.position - forward * Offset.z + Vector3.up * Offset.y;
-                Vector3 targetPosition = TargetToFollow.position - forward;
-
-                // Lerp seems to be smoother than the SmootDamp
-                //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
-                transform.position = Vector3.Lerp(transform.position, targetPosition, SmoothTime);
-                transform.rotation = TargetToFollow.rotation;
-
-                // LookAt is causing the camera to flip, I'll keep it commented here
-                //transform.LookAt(TargetToFollow);
+                return;
             }
+
+            Vector3 forward = TargetToFollow.forward * TargetDistance;
+            Vector3 targetPosition = TargetToFollow.position - forward + Offset;
+
+            // Lerp seems to be smoother than the SmootDamp
+            //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, SmoothTime);
+            transform.rotation = TargetToFollow.rotation;
+
+            // LookAt is causing the camera to flip, I'll keep it commented here
+            //transform.LookAt(TargetToFollow);
         }
     }
 }
--- Assets/Scripts/Game/UI/Util/FPSCounter.cs
+++ Assets/Scripts/Game/UI/Util/FPSCounter.cs
@@ -1,11 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace FinalFrontier.Game.UI.Util
 {
-    public class FPSCounter : MonoBehaviour 
+    // ReSharper disable once InconsistentNaming
+    [RequireComponent(typeof(Text))]
+    public class FPSCounter : MonoBehaviour
     {
+        private Text _fpsText;
+        private int _fpsValue;
         
+        private readonly string[] _stringsFrom00To99 = {
+            "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+            "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+            "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+            "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+            "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+            "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+            "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+            "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+            "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+            "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+        };
+
+        private void Awake()
+        {
+            _fpsText = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            _fpsValue = (int) (1f / Time.unscaledDeltaTime);
+            _fpsText.text = _stringsFrom00To99[Mathf.Clamp(_fpsValue, 0, 90)];
+        }
     }
 }
