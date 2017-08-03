25614116e262058643ab0f9278ab077b58ab6eda
Bruno Cicanci <brunocicanci@gmail.com>
2017-08-02 22:57:18 -0300

    Spaceship movement using mouse and screen limits

--- Assets/Scripts/Game/Spaceship/SpaceshipControl.cs
+++ Assets/Scripts/Game/Spaceship/SpaceshipControl.cs
@@ -5,6 +5,17 @@ namespace Game.Spaceship
     public class SpaceshipControl : MonoBehaviour 
     {
         public float MovementSpeed;
+        
+        [Range(0, 1)]
+        public float MovementLimitTop = 0.7f;
+        [Range(0, 1)]
+        public float MovementLimitBottom = 0.3f;
+        [Range(0, 1)]
+        public float MovementLimitLeft = 0.3f;
+        [Range(0, 1)]
+        public float MovementLimitRight = 0.7f;
+
+        private bool _buttonPressed;
 
         private void Update() 
         {
@@ -16,6 +27,38 @@ namespace Game.Spaceship
         {
             float horizontalAxis = Input.GetAxis("Horizontal");
             float verticalAxis = Input.GetAxis("Vertical");
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                _buttonPressed = true;
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                _buttonPressed = false;
+            }
+
+            if(_buttonPressed)
+            {
+                var mousePosition = Input.mousePosition;
+                if (mousePosition.x < Screen.width * MovementLimitLeft)
+                {
+                    horizontalAxis = -1;
+                }
+                else if (mousePosition.x > Screen.width * MovementLimitRight)
+                {
+                    horizontalAxis = 1;
+                }
+
+                if(mousePosition.y < Screen.height * MovementLimitBottom)
+                {
+                    verticalAxis = -1;
+                }
+                else if(mousePosition.y > Screen.height * MovementLimitTop)
+                {
+                    verticalAxis = 1;
+                }
+            }
             
             float h = horizontalAxis * Time.deltaTime * MovementSpeed;
             float v = verticalAxis * Time.deltaTime * -MovementSpeed;
